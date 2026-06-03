49d2bbffc2682569dc98a22a012ac1318085bf81
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-06-03 14:45:35 +0900

    Update student.h

--- student.h
+++ student.h
@@ -9,18 +9,18 @@ typedef struct Student {
 } Student; //Student 구조체 선언
 
 
-int add(Student** head, int id, const char* name, int score); //학생 추가
+int S_add(Student** head, int id, const char* name, int score); //학생 추가
 
-int delete(Student** head, int id); //학생 삭제
+int S_delete(Student** head, int id); //학생 삭제
 
-Student* find(Student* head, int id); //학생 검색
+Student* S_find(Student* head, int id); //학생 검색
 
-int update(Student* head, int id, int score); //점수 설정
+int S_update(Student* head, int id, int score); //점수 설정
 
-void list(Student* head); //전체 학생 출력
+void S_list(Student* head); //전체 학생 출력
 
-void stats(Student* head); //통계 출력
+void S_stats(Student* head); //통계 출력
 
-void free_memory(Student** head); //동적 할당 메모리 해제
+void S_free_memory(Student** head); //동적 할당 메모리 해제
 
 #endif
